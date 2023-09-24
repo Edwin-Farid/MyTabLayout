f5417dacff3edade851feaabcf77de9941f0a5d7
Edwin-Farid <117edwin@gmail.com>
2023-09-24 22:55:24 +0800

    feat: add condition fragment

--- app/src/main/java/com/dicoding/mytablayout/SectionsPagerAdapter.kt
+++ app/src/main/java/com/dicoding/mytablayout/SectionsPagerAdapter.kt
@@ -6,10 +6,15 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return 2
     }
 
     override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment = ProfileFragment()
+        }
+        return fragment as Fragment
     }
 }
