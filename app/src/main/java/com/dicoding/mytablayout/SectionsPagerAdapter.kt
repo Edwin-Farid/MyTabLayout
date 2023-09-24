545a3217fdb855566f0eb1b464b0dff74a391fe6
Edwin-Farid <117edwin@gmail.com>
2023-09-24 22:52:54 +0800

    feat: add fragment adapter

---
+++ app/src/main/java/com/dicoding/mytablayout/SectionsPagerAdapter.kt
@@ -0,0 +1,7 @@
+package com.dicoding.mytablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+}
